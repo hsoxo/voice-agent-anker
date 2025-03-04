475fb550be412082199a81d496b2fe29c7abd9cb
Bolong <ihebolong@gmail.com>
2025-03-04 21:01:52 +0000

    add proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {  
+  async rewrites() {
+    return [
+      {
+        source: "/voice-api/:path*", 
+        destination: "http://44.213.201.101:7860/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
