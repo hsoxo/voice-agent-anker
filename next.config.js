fdf31089fbb72feaad01d471c1743552290f60d7
HHe <ihebolong@gmail.com>
2025-06-15 20:24:01 -0700

    1

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/static/chunks/remoteEntry.js",
+        source: "/_next/static/chunks/remoteEntry.js",
         headers: [
           {
             key: "Cache-Control",
